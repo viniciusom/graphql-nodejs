19d8bf2ef96e5be3f514f53aa4873f025ccd2b2a
Vinicius Moraes <viniciusom01@hotmail.com>
2021-01-23 17:17:25 -0300

    create first mutation

--- src/index.js
+++ src/index.js
@@ -8,15 +8,58 @@ const { ApolloServer, gql } = require('apollo-server');
 // Scalar types => String, Int, Boolean, Float, ID
 
 const typeDefs = gql`
+  type User {
+    _id: ID!
+    name: String!
+    email: String!
+    active: Boolean!
+  }
+  
+  type Post {
+    _id: ID!
+    title: String!
+    email: String!
+    author: User!
+  }
+
   type Query {
     hello: String
+    users: [User!]!
+    getUserByEmail(email: String!): User!
+  }
+
+  type Mutation {
+    createUser(name: String!, email: String!): User!
   }
 `;
 
+const users = [
+  { _id: String(Math.random()), name: 'Vinicius', email: 'viniciusom01@hotmail.com', active: true },
+  { _id: String(Math.random()), name: 'Manoel', email: 'manoelsilva@hotmail.com', active: false },
+  { _id: String(Math.random()), name: 'José', email: 'josealberto@hotmail.com', active: true },
+]
+
+
 const resolvers = {
   Query: {
-    hello: () => 'Hello world'
-  }
+    hello: () => 'Hello world',
+    users: () => users,
+    getUserByEmail: (_, args) => {
+      return users.find((user) => user.email === args.email);
+    }
+  },
+  Mutation: {
+    createUser: (_, args) => {
+      const newUser = {
+        _id: String(Math.random()),
+        name: args.name, 
+        email: args.email,
+        active: true,
+      };
+      users.push(newUser);
+      return newUser;
+    }
+  },
 };
 
 const server = new ApolloServer({ typeDefs, resolvers });
